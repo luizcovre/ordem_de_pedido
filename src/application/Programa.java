a02415d9d67ac0a1c74d2e000bfc9f5cdd384e20
luizcovre <luiz.1covre@gmail.com>
2021-01-05 16:55:10 -0300

    Classe Cliente

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -3,21 +3,12 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.ItemDoPedido;
-import entities.Produto;
-
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner read = new Scanner(System.in);
-		
-		Produto p = new Produto("Televisor", 1000.00);
-		
-		ItemDoPedido ip_01 = new ItemDoPedido(1, 1000.00, p);
-		
-		System.out.println(ip_01);
 		
 		read.close();
 	}
